736d7ff8315fae78f51df405d4b6eefeacab7452
EdwinSanti <edwinalexiss12gmail.com>
2026-01-28 19:41:07 -0400

    updates

--- model.py
+++ model.py
@@ -1,12 +1,21 @@
 # model.py
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Input
+from keras.layers import ( Dense, Conv2D, Flatten, MaxPooling2D, Input,
+ RandomFlip, RandomTranslation, RandomRotation, RandomZoom )
+
 from config import INPUT_SHAPE, NUM_CLASSES
 
 
 def build_model():
     model = Sequential([
         Input(shape=INPUT_SHAPE),
+# Data Augmentation
+        RandomFlip("horizontal"),
+        RandomTranslation(0.1, 0.1),
+        RandomRotation(0.1),
+        RandomZoom(0.1),
+        
+        
         Conv2D(32, kernel_size=(3, 3), activation="relu"),
         MaxPooling2D(pool_size=(2, 2)),
         Flatten(),
--- train.py
+++ train.py
@@ -1,13 +1,30 @@
+
+from keras.callbacks import ReduceLROnPlateau
+
+
+
 # train.py
 def compile_model(model):
     model.compile(
-        optimizer="SGD",
+        optimizer="adam", 
         loss="SparseCategoricalCrossentropy",
         metrics=["accuracy"],
     )
     return model
-
-
+                           #changed from 'sgd' to Adam adapts the learning rate
+                           # each parameter individually, allowing the model to 
+                           #converge faster and achieve better early accuracy than
+                           #standard SGD, especially on datasets like CIFAR-10 with small CNNs.
+lr_scheduler = ReduceLROnPlateau(
+    monitor="val_loss",
+    factor=0.5,
+    patience=3,
+    min_lr=1e-6,
+    verbose=1,
+)
+                           # reduce learning rate when a metric has stopped improving
+                           #  can help the model slow down and fine tune.
+    
 def train_model(model, x_train, y_train, batch_size, epochs):
     history = model.fit(
         x_train,
@@ -15,6 +32,7 @@ def train_model(model, x_train, y_train, batch_size, epochs):
         batch_size=batch_size,
         epochs=epochs,
         validation_split=0.1,
+        callbacks=[lr_scheduler],
     )
     return history
 
