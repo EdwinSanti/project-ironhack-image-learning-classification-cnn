d7e53425a1a66fe5ad2b91759b282dcdc7c9b9af
EdwinSanti <edwinalexiss12gmail.com>
2026-01-29 07:21:50 -0400

    increased model arch - added data augment - train.py

--- model.py
+++ model.py
@@ -1,10 +1,16 @@
 # model.py
 from keras.models import Sequential
-from keras.layers import ( Dense, Conv2D, Flatten, MaxPooling2D, Input,
- RandomFlip, RandomTranslation, RandomRotation, RandomZoom )
+from keras.layers import (
+     Input, Conv2D, MaxPooling2D, Dense,
+    RandomFlip, RandomTranslation, RandomRotation, RandomZoom,
+    BatchNormalization, Dropout, GlobalAveragePooling2D)
 
 from config import INPUT_SHAPE, NUM_CLASSES
 
+#The model was improved by adding Batch Normalization and Dropout layers 
+# and increasing convolutional depth, which stabilizes training, reduces 
+# overfitting, and allows the network to learn more robust features for 
+# better generalization on CIFAR-10.
 
 def build_model():
     model = Sequential([
@@ -16,10 +22,19 @@ def build_model():
         RandomZoom(0.1),
         
         
-        Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        Conv2D(64, kernel_size=(3, 3), padding="same", activation="relu"),
+        BatchNormalization(),
         MaxPooling2D(pool_size=(2, 2)),
-        Flatten(),
-        Dense(100, activation="relu"),
+        Dropout(0.15),
+
+        Conv2D(64, kernel_size=(3, 3), padding="same", activation="relu"),
+        BatchNormalization(),
+        MaxPooling2D(pool_size=(2, 2)),
+        Dropout(0.15),
+
+        GlobalAveragePooling2D(),
+        Dense(128, activation="relu"),
+        Dropout(0.3),
         Dense(NUM_CLASSES, activation="softmax"),
     ])
     return model
